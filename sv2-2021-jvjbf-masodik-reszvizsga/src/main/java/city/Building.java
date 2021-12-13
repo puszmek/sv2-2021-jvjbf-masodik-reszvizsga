6051e39ce1abeaa6a06d0e1a4a3c0ecf11cc34ab
he <bgyhenrietta@yahoo.com>
2021-12-13 18:48:28 +0100

    done

---
+++ sv2-2021-jvjbf-masodik-reszvizsga/src/main/java/city/Building.java
@@ -0,0 +1,38 @@
+package city;
+
+public abstract class Building {
+
+    private int area;
+    private int levels;
+    private Address address;
+
+    public Building(int area, int levels, Address address) {
+        this.area = area;
+        this.levels = levels;
+        this.address = address;
+    }
+
+    public Building(int area, Address address) {
+        this.area = area;
+        this.address = address;
+        levels = 1;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public int getLevels() {
+        return levels;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public int getFullArea() {
+        return area * levels;
+    }
+
+    public abstract int calculateNumberOfPeopleCanFit();
+}
